0f20c0c9582eeb2b445e94b4c048a42cddddf712
Lais <Laaia@hotmail.com>
2014-04-27 17:49:05 -0300

    atualização parametros

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
 #include "func.h"
 
-int main(){
-
+int main(int rg, char *arq[]){
     //main demonstrativa, para mais detalhes consulte arquivo LEIAME.txt e func.c
+
+    char *meta = arq[1], *data = arq[2]; //Faça a atribuição conforme a ordem de entrada na execução pela linha de comando
+
     struct page *buffer = inicializaBuffer();
 
-	struct CAMPOS *campos = leMetadados("meta.dat"); // Traz pra memória o esquema dos dados
+	struct CAMPOS *campos = leMetadados(meta); // Traz pra memória o esquema dos dados
 
-    carregaDados(buffer, "data.dat", "meta.dat", campos);
+    carregaDados(buffer, data, meta, campos);
 
     //showBuffer(buffer, campos, "meta.dat");
 
-    showTupleBuffer(buffer, campos, "meta.dat", 1, 2);
+    showTupleBuffer(buffer, campos, meta, 0, 1);
 
 
     return 0;
